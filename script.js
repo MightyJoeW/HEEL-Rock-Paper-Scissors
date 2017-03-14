4cfd8f8c338bc6d8ac5591c551ffdb09ecb1f22b
Joe <themightyjoew@gmail.com>
2017-03-14 14:01:44 -0500

    HEEL Rock Paper Scissors 2.0
    Complete overhaul with better styling and full functionality.

--- script.js
+++ script.js
@@ -1,44 +1,89 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-// var userChoice = document.getElementById("playerOne").value;
-var computerChoice = Math.random(); //Math.random() randomly picks a number 0-1
- 
-    if (computerChoice < 0.34) {
-       computerChoice = "rock";
-   } else if(computerChoice <= 0.67) {
-       computerChoice = "paper";
-   } else {
-       computerChoice = "scissors";
-   } document.write("You: " + userChoice + " vs. Computer: " + computerChoice + ". ");
+var loses = 0;
+var wins = 0;
 
-   var compare = function(choice1, choice2) {
+var play = function(userChoice) {
+
+    document.getElementById("player").innerHTML="";
+    document.getElementById("opponent").innerHTML="";
+    document.getElementById("results").innerHTML="";
+
+
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        document.getElementById("player").innerHTML='You chose ' + userChoice + '.';
+    } else {
+        document.getElementById("player").innerHTML="That is not a valid choice. Try again, jabroni!";
+
+        return false;
+    }
+
+var computerChoice = Math.random(); //Math.random() randomly picks a number 0-1
+
+if (computerChoice < 0.34) {
+   computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+   computerChoice = "paper";
+} else {
+   computerChoice = "scissors";
+}
+
+document.getElementById("opponent").innerHTML='// Your opponent chose ' + computerChoice + '.';
+
+var compare = function(choice1, choice2) {
     if (choice1 === choice2) {
-        document.write("We don't end in ties around here. Choose again, jabroni!");
+        return("We don't end in ties around here. Choose again, jabroni!");
     }
     else if (choice1 === "rock") {
         if (choice2 === "scissors") {
-            document.write("IT DOESN'T MATTER if you chose rock! Computer will take their scissors, shine them up real nice, turn them sideways, and stick them straight up your fake rock!");
+            loses++;
+            return("IT DOESN'T MATTER if you chose rock! Computer will take their scissors, shine them up real nice, turn them sideways, and stick them straight up your fake rock!");
         }
         else {
-            document.write("How dare you try to choose rock! Computer laid the smackdown with paper! You lose...");
+            loses++;
+            return("How dare you deem yourself worthy enough to choose rock! Computer laid the smackdown with paper! You lose...");
         }
     }
     else if (choice1 === "paper") {
         if (choice2 === "rock") {
-            document.write("IT DOESN'T MATTER if computer chose rock and you chose paper! You still lose.");
+            loses++;
+            return("IT DOESN'T MATTER if computer chose rock and you chose paper! You still lose.");
         }
         else {
-            document.write("Computer chose scissors which means you've been checked into Smackdown Hotel. You LOSE!");
+            loses++;
+            return("You've been checked into Smackdown Hotel. You LOSE!");
         }
     }
     else if (choice1 === "scissors") {
         if (choice2 === "rock") {
-            document.write("Computer chose rock which results in a Rock Bottom AND People's Elbow. 1-2-3. YOU LOSE!");
+            loses++;
+            return("Computer smelled what rock was cooking which results in a Rock Bottom AND People's Elbow. 1-2-3. YOU LOSE!");
         }
         else {
-            document.write("Computer chose Paper. Since neither of you smelled what rock was cooking, you BOTH LOSE. ");
+            loses++;
+            return("Since you decided NOT to smell what rock was cooking, you still LOSE!");
         }
     }
-
 };
 
-compare(userChoice, computerChoice);
+var winner = compare(userChoice,computerChoice);
+document.getElementById("results").innerHTML=winner;
+document.getElementById("wins").innerHTML=wins;
+document.getElementById("loses").innerHTML=loses;
+
+if (wins > 99 || loses > 99) {
+    document.getElementById("wins").style.fontSize="44";
+    document.getElementById("loses").style.fontSize="44";
+}
+if (wins > 999) {
+    alert("You reached the max score of 999. <br />Congratulations, you are the new People's Champion.");
+}
+if (loses > 999) {
+    alert("Your opponent reached the max score of 999. <br />You ought to be ashamed of yourself, jobber...");
+}
+};
+
+var reset = function() {
+    loses = 0;
+    wins = 0;
+    document.getElementById("wins").innerHTML=wins;
+    document.getElementById("loses").innerHTML=loses;
+};
